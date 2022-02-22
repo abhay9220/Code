8bcb6e7c79d9f190b31bb19a92ef843d1000ef3d
Abhay <lahareabhay00@gmail.com>
2022-02-22 20:56:22 +0530

    code8.py added

---
+++ code8.py
@@ -0,0 +1,55 @@
+# Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+# Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+# Example:
+#      records=[["chi",20.0], ["beta", 50.0], ["alpha",50.0]]
+
+
+# The ordered list of scores is [20.0,50.0] , so the second lowest score is 50.0. There are two students with that score: ["alpha", "beta"]. Ordered alphabetically, the names are printed as:  
+#          alpha
+#          beta
+
+
+
+# Input Format:
+#          The first line contains an integer, N , the number of students.
+#          The  subsequent 2N lines describe each student over 2 lines.
+#                       - The first line contains a student's name.
+#                       - The second line contains their grade.
+
+# Constraints:
+#           2<=N<=5
+
+# There will always be one or more students having the second lowest grade.
+
+
+
+# Output Format:
+#            Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically and print each one on a new line.
+
+
+if __name__ == '__main__':
+    student = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        student.append([name,score])
+    
+    x = 999
+    for i in range(len(student)):
+        if x > float(student[i][1]):
+            x = float(student[i][1])
+    y = 999
+    for i in range(len(student)):
+        if float(student[i][1]) > float(x) and y > float(student[i][1]):
+            y = float(student[i][1])
+    runner = []
+    for i in range(len(student)):
+        if float(student[i][1]) == float(y):
+            runner.append(student[i][0])
+    runner = sorted(runner)
+
+    for i in range(len(runner)):
+        print(runner[i])
+        
